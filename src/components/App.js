fd7e0daa8cf973ef00f8166b1fb29628aef51cc7
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-28 15:41:30 -0700

    Bunch of stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../styles/App.css';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LinkList from './LinkList';
+import CreateLink from './CreateLink';
+import Header from './Header';
+import Login from './Login';
+import Search from './Search';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='center w85'>
+        <Header />
+        <div className='ph3 pv1 background-gray'>
+          <Switch>
+            <Route exact path='/' component={LinkList} />
+            <Route exact path='/create' component={CreateLink} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/search' component={Search} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
